c34b9af2fb66499f7431f1f5f192faf76ae68c4e
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-24 23:49:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  my_array = []
 end
 
 def create_an_array
-  
+  my_array = [1, "two", 3, "four"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ 
 end
 
 def add_element_to_start_of_array(array, element)
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  .index("0")
 end
 
 def retrieve_last_element_from_array(array)
