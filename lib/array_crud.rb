03abbf07dd4bf337c89b9bdf75578c20788f0fd8
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-25 00:30:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  my_array = []
+  puppies = []
 end
 
 def create_an_array
-  my_array = [1, "two", 3, "four"]
+  puppies = ["marshall", "chase", "rocky", "rubble","sky"]
 end
 
 def add_element_to_end_of_array(array, element)
- 
+  puppies.push "zuma"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  puppies.unshift "ryder"
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ryder = puppies.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  marshall = puppies.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  puppies [1]
 end
 
 def retrieve_first_element_from_array(array)
-  .index("0")
+  puppies.index("marshall")
 end
 
 def retrieve_last_element_from_array(array)
-  
+  puppies.last
 end
